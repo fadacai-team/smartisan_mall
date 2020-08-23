81b60a5ae41058108679e8d3fdb111e12a9a9d9d
liudiming <1378542732@qq.com>
2020-08-23 13:19:36 +0800

    完成搜索框

--- src/Utils/myaxios.js
+++ src/Utils/myaxios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL:'https://shopapi.smartisan.com/',
+    // baseURL:'https://shopapi.smartisan.com/',
     timeout:1000
 })
 // 请求 @todo 登录之后的 auth_token 要附带在 config.data 里面
