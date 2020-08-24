de69e0bdef191abd94b1d9c8a1d0e125b425b4ad
chamsonxie <chamson1995@qq.com>
2020-08-24 21:06:18 +0800

    123

--- src/Router/config.js
+++ src/Router/config.js
@@ -6,7 +6,6 @@ import Home from '../components/Home/Home'
 import Category from '../components/Category/Category'
 import Cart from '../components/Cart/Cart'
 import Regist from '../components/My/regist/Regist'
-import SearchBar from '../components/SearchBar/SearchBar'
 const routes = [
     {
         path:'/list',
