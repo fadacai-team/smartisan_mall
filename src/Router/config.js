253f055474c2f6ad85159a23fe99988ba2f3add3
liudiming <1378542732@qq.com>
2020-08-24 21:52:39 +0800

    解决bug

--- src/Router/config.js
+++ src/Router/config.js
@@ -1,10 +1,6 @@
-// import Home from '../components/Home/Home'
 import List from '../components/list/List'
 import SearchBar from '../components/SearchBar/SearchBar'
 import Detail from '../components/Datail'
-import Home from '../components/Home/Home'
-import Category from '../components/Category/Category'
-import Cart from '../components/Cart/Cart'
 import Regist from '../components/My/regist/Regist'
 const routes = [
     {
@@ -23,20 +19,8 @@ const routes = [
         children:[]
     },
     {
-        path:'/home',
-        component:Home,
-     
-    },{
-        path:'/cart',
-        component:Cart,
-        
-    },
-    {
         path:'/my/regist',
         component:Regist,
-    },{
-        path:'/search',
-        component:SearchBar
     }
 ]
 
